27143077fa94115100f8d1ef3667738c28ace478
Raditya Harya <radityaharya02@gmail.com>
2023-09-25 12:13:13 +0000

    add: youtube as background component

---
+++ app/components/YtPlayer_client.tsx
@@ -0,0 +1,69 @@
+"use client";
+import YouTube, { YouTubeProps } from "react-youtube";
+import { useState, useEffect } from "react";
+
+interface Props {
+  videoId?: string;
+  className?: string;
+}
+
+export const YoutubePlayer: React.FC<Props> = ({ videoId, ...Props }) => {
+  const [hidden, setHidden] = useState(true);
+  const [clientSize, setClientSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setClientSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const opts: YouTubeProps["opts"] = {
+    height: clientSize.height.toString(),
+    width: clientSize.width.toString(),
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      mute: 1,
+      controls: 0,
+      loop: 1,
+      iv_load_policy: 3,
+    },
+  };
+
+  const onReady = (event: any) => {
+    setTimeout(() => {
+      setHidden(false);
+    }, 4000);
+  };
+
+  const onEnd = (event: any) => {
+    event.target.playVideo();
+  };
+
+  return (
+    <div className="absolute top-0 left-0 max-w-screen max-h-screen w-full h-full overflow-hidden">
+      <div className={`absolute top-0 left-0 z-10 w-full h-full bg-black opacity-50 ${hidden ? "opacity-0" : "opacity-100"} transition-opacity duration-1000 ease-in-out`} />
+      <YouTube
+        videoId={videoId ?? "4IlF715Yn00"}
+        opts={opts}
+        onReady={onReady}
+        onEnd={onEnd}
+        className={`absolute top-0 left-0 w-full h-full overflow-hidden transform ${
+          hidden ? "opacity-0" : "opacity-100"
+        } transition-opacity duration-1000 ease-in-out ${hidden ? "scale-100" : "scale-110"} transition-transform duration-1000 ease-in-out ${Props.className}`}
+        style={{ pointerEvents: "none" }}
+      />
+    </div>
+  );
+};

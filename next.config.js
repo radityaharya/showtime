b99294045bbd5ccad5601245d8af00039d664946
Raditya Harya <radityaharya02@gmail.com>
2023-10-10 08:49:19 +0000

    unoptimized image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     domains: [
       "user-images.githubusercontent.com",
       "s3.us-west-2.amazonaws.com",

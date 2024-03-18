5050f96597f0e3693721b5529660cd823ce96fcb
Raditya Harya <radityaharya02@gmail.com>
2024-03-18 12:41:06 +0000

    Add Redis caching for calendar routes

--- app/api/user/[uid]/calendar/movies/ical/route.ts
+++ app/api/user/[uid]/calendar/movies/ical/route.ts
@@ -2,6 +2,7 @@ import { TraktAPI } from "@/lib/trakt/Trakt";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import * as Sentry from "@sentry/nextjs";
+const Redis = require("ioredis");
 
 export async function GET(
   request: NextRequest,
@@ -32,11 +33,30 @@ export async function GET(
       throw new Error("days_ago and period must be safe integers");
     }
 
+    const redis_client = new Redis(process.env.REDIS_URL, {
+      keepAlive: 5000,
+    });
+
+    const cache_key = `GET:${request.url}`;
+
+    const cachedResponse = await redis_client.get(cache_key);
+    if (cachedResponse) {
+      const ttl = await redis_client.ttl(cache_key);
+      console.info(`Cache used for request: ${request.url}. TTL: ${ttl} seconds`);
+      const cal = new Blob([cachedResponse], { type: "text/calendar" });
+      return new NextResponse(cal, {
+        headers: {
+          "Content-Type": "text/calendar",
+          "Content-Disposition": `attachment; filename="trakt-${
+            params.uid
+          }-${new Date().toISOString()}.ics"`,
+        },
+      });
+    }
+
     const trakt = new TraktAPI(undefined, params.uid);
-    const cal = new Blob(
-      [(await trakt.Movies.getMoviesCalendar(days_ago, period)).toString()],
-      { type: "text/calendar" },
-    );
+    const calendarData = await trakt.Movies.getMoviesCalendar(days_ago, period);
+    const cal = new Blob([calendarData.toString()], { type: "text/calendar" });
 
     Sentry.setUser({
       username: params.uid,
@@ -47,7 +67,7 @@ export async function GET(
       searchParams: Object.fromEntries(request.nextUrl.searchParams),
     });
 
-    return new NextResponse(cal, {
+    const response = new NextResponse(cal, {
       headers: {
         "Content-Type": "text/calendar",
         "Content-Disposition": `attachment; filename="trakt-${
@@ -55,6 +75,10 @@ export async function GET(
         }-${new Date().toISOString()}.ics"`,
       },
     });
+
+    await redis_client.set(cache_key, calendarData.toString(), "EX", 18000);
+
+    return response;
   } catch (error) {
     console.error(error instanceof Error ? error.message : error);
     Sentry.setContext("request", {
